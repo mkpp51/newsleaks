ef57f9106464d9c708a319ad0a0b2ee47961c04f
mkpp51 <117806853+mkpp51@users.noreply.github.com>
2022-12-12 10:15:32 +0300

    -

--- NewsPortal/news/views.py
+++ NewsPortal/news/views.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from django.views.generic import ListView, DetailView
 from .models import Post
 
@@ -8,11 +6,6 @@ class PostList(ListView):
     model = Post
     template_name = 'news.html'
     context_object_name = 'news'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['time_now'] = datetime.utcnow()
-        return context
 
 
 class PostDetail(DetailView):
