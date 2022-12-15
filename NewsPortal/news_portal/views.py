a4906a8a83ae08a53fc25c11d54bbbf9822ffb1c
mkpp51 <117806853+mkpp51@users.noreply.github.com>
2022-12-15 16:04:49 +0300

    -

--- NewsPortal/news_portal/views.py
+++ NewsPortal/news_portal/views.py
@@ -1,9 +1,6 @@
 from datetime import datetime
 
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.urls import reverse_lazy
-from django.utils.decorators import method_decorator
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, TemplateView
 
 from .filters import PostFilter
