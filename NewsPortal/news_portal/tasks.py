a7fe15bf65c824874f222a7ad84dee90ca91607b
mkpp51 <117806853+mkpp51@users.noreply.github.com>
2023-07-17 09:11:10 +0300

    -

--- NewsPortal/news_portal/tasks.py
+++ NewsPortal/news_portal/tasks.py
@@ -30,29 +30,23 @@ def notification_sender(pk):
 
 @shared_task
 def weekly_updates_sender():
-    emails = {}
+    subscribers_emails = []
+    articles_list = []
     week_ago = datetime.now() - timedelta(days=7)
-    url = 'http://127.0.0.1:8000'
 
     for category in Category.objects.all():
+        subscribers = category.subscribers.all()
         _category = category.cat_name.title()
         articles = Post.objects.filter(post_cat=category,
                                        post_pub_date__gte=week_ago)
-        if not articles:
-            continue
-        for user in category.subscribers.all():
-            if user not in emails:
-                emails[user] = {}
-            if _category not in emails[user]:
-                emails[user][_category] = set()
-            emails[user][_category].update(articles)
+        articles_list.append(articles)
+        for user in subscribers:
+            subscribers_emails.append(user.email)
 
-    for user, categories in emails.items():
-        message = []
-        for category, articles in categories.items():
-            message.extend((category, *(
-                f'{article.post_header}: {url}/{article.get_absolute_url()}'
-                for article in articles)))
-
-        send_mail('New articles of this week', '\n'.join(message), None,
-                  [user.email])
+    send_mail(
+        subject=f'NEWSPORTAL. Weekly publications compilation.',
+        message=f'Weekly update on publication(s) in your favorite category(ies):\n'
+                f'{articles_list}',
+        from_email=settings.SERVER_EMAIL,
+        recipient_list=subscribers_emails
+    )
